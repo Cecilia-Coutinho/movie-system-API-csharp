24c4c61a166329e949969448a652137f53b05e60
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-04-18 11:16:52 +0200

    Created

--- Models/PersonMovie.cs
+++ Models/PersonMovie.cs
@@ -1,6 +1,24 @@
-﻿namespace MovieSystemAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieSystemAPI.Models
 {
     public class PersonMovie
     {
+        [Key]
+        public int PersonMovieId { get; set; }
+
+        [Required]
+        public int FkPersonGenreId { get; set; }
+
+        [Required]
+        public string MovieLink { get; set; } = string.Empty;
+
+        public int PersonRating { get; set; }
+
+        public int MovieAverageRate { get; set; }
+
+        [JsonIgnore]
+        public virtual PersonGenre PersonGenre { get; set; }
     }
 }
