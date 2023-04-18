20a6dfa41969ca939cd79c67e1fdf67fd9e06512
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-04-18 11:15:23 +0200

    Added v.1

---
+++ Controllers/PersonGenreController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieSystemAPI.Models;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonGenreController : ControllerBase
+    {
+
+        private readonly DatabaseContext _context;
+
+        public PersonGenreController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<PersonGenreController>
+        [HttpGet]
+        public async Task<ActionResult<List<PersonGenre>>> GetAll()
+        {
+            return Ok(await _context.PersonGenres.ToListAsync());
+        }
+
+        // GET api/<PersonGenreController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PersonGenre>> GetById(int id)
+        {
+            var personGenre = await _context.PersonGenres.FindAsync(id);
+
+            if (personGenre == null)
+            {
+                return BadRequest("Person Genre not found");
+            }
+            return Ok(personGenre);
+        }
+
+        // POST api/<PersonGenreController>
+        [HttpPost("{personId}/{genreId}")]
+        public async Task<ActionResult<List<PersonGenre>>> AddPersonGenre(int personId, int genreId)
+        {
+            var person = await _context.People.FindAsync(personId);
+
+            if (person == null)
+            {
+                return BadRequest($"Person with ID {personId} not found");
+            }
+
+            var genre = await _context.Genres.FindAsync(genreId);
+            if (genre == null)
+            {
+                return BadRequest($"Genre with ID {genreId} not found");
+            }
+
+            // check if person already has the genre
+            var existingPersonGenre = await _context.PersonGenres.Where(
+                p => p.FkPersonId == personId &&
+                p.FkGenreId == genreId).ToListAsync();
+
+            if (existingPersonGenre.Count > 0)
+            {
+                return BadRequest("This combination already exists in the database");
+            }
+
+            // Create a new PersonGenre object and add it to the database
+            PersonGenre personGenre = new PersonGenre()
+            {
+                FkPersonId = personId,
+                FkGenreId = genreId
+            };
+
+            _context.PersonGenres.Add(personGenre);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.PersonGenres.ToListAsync());
+        }
+
+    }
+}
