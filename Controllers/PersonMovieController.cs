46c0e40324ca9f9dc98cdd5d7a654be66a6df0fd
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-04-19 17:31:13 +0200

    added

--- Controllers/PersonMovieController.cs
+++ Controllers/PersonMovieController.cs
@@ -20,36 +20,98 @@ namespace MovieSystemAPI.Controllers
             _context = context;
         }
 
-        // GET: api/<PersonMovieController>
+        // GET
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<List<PersonMovie>>> GetAll()
         {
-            return new string[] { "value1", "value2" };
+            var personMovieList = await _context.PersonMovies.ToListAsync();
+
+            if (personMovieList == null)
+            {
+                return BadRequest($"PersonMovie is empty.");
+            }
+
+            return Ok(personMovieList);
         }
 
-        // GET api/<PersonMovieController>/5
+        [HttpGet("{personId}")]
+        public async Task<ActionResult<PersonMovie>> GetByPersonId(int personId)
+        {
+
+            var person = await _context.People.FindAsync(personId);
+
+            if (person == null)
+            {
+                return BadRequest($"Person with ID {personId} not found");
+            }
+
+            var movies = await _context.Movies
+                .Join(_context.PersonMovies,
+                    g => g.MovieId,
+                    pg => pg.FkMovieId,
+                    (g, pg) => new { Movie = g, PersonMovie = pg })
+                .Where(p => p.PersonMovie.FkPersonId == personId)
+                .Select(g => g.Movie.MovieTitle)
+                .ToListAsync();
+
+            if (!movies.Any())
+            {
+                return BadRequest($"No Movies found");
+            }
+
+            return Ok(movies);
+        }
+
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<PersonMovie>> GetById(int id)
         {
-            return "value";
+            var personMovie = await _context.PersonMovies.FindAsync(id);
+
+            if (personMovie == null)
+            {
+                return BadRequest("Person Movie not found");
+            }
+            return Ok(personMovie);
         }
 
-        // POST api/<PersonMovieController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
 
-        // PUT api/<PersonMovieController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPost("{personId}/{movieId}")]
+        public async Task<ActionResult<List<PersonMovie>>> AddPersonMovie(int personId, int movieId)
         {
-        }
+            var person = await _context.People.FindAsync(personId);
 
-        // DELETE api/<PersonMovieController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            if (person == null)
+            {
+                return BadRequest($"Person with ID {personId} not found");
+            }
+
+            var movie = await _context.Movies.FindAsync(movieId);
+            if (movie == null)
+            {
+                return BadRequest($"Genre with ID {movieId} not found");
+            }
+
+            // check if person already has the movie
+            var existingPersonMovie = await _context.PersonMovies.Where(
+                p => p.FkPersonId == personId &&
+                p.FkMovieId == movieId).ToListAsync();
+
+            if (existingPersonMovie.Count > 0)
+            {
+                return BadRequest("This combination already exists in the database");
+            }
+
+            // Create a new PersonGenres object and add it to the database
+            PersonMovie personGenre = new PersonMovie()
+            {
+                FkPersonId = personId,
+                FkMovieId = movieId
+            };
+
+            _context.PersonMovies.Add(personGenre);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.PersonMovies.ToListAsync());
         }
     }
 }
